672de97fdacb9b9b2418d914ad8dfdf1ba030968
Vladimir Ladygin <ladygin@lenvoku.ru>
2022-08-25 14:05:54 +0300

    Add to change many things in storyboard, connect buttons from storyboard with code. I did all tasks.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let eggTimes = [ "Soft": 300, "Medium": 420, "Hard": 720]
+    
+    var secondRemaining = 60
+    
+    var timer = Timer()
 
-
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+      let hardness = sender.currentTitle! //Soft, Medium, Hard
+        
+        timer.invalidate()
+        
+        secondRemaining = eggTimes[hardness]!
+        
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        if secondRemaining > 0 {
+            print("\(secondRemaining) seconds")
+            secondRemaining -= 1
+                        
+        }
+    }
 }
